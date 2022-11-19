0430711e439ae1ee4a9e859510c4e7b80fafebd1
RakeshKumarParida <rakeshkumarparida424@gmail.com>
2022-11-19 16:59:25 +0530

    Added some animations

--- script.js
+++ script.js
@@ -17,7 +17,270 @@ calculate.addEventListener('click', function (e) {
     
     inputBox.value = eval(inputBox.value);
     
-})
+});
+
+let allbtns = document.getElementsByClassName('btno');
+
+
+for (let i = 0; i < allbtns.length; i++){
+  // Adding class
+
+  allbtns[0].addEventListener("mousedown", function () {
+    allbtns[0].classList.add("btnActive");
+    allbtns[0].classList.add("fontActive");
+    allbtns[0].classList.add("greenActive");
+  });
+
+  // removing class
+
+  allbtns[0].addEventListener("mouseup", function () {
+    allbtns[0].classList.remove("btnActive");
+    allbtns[0].classList.remove("fontActive");
+    allbtns[0].classList.remove("greenActive");
+  });
+
+  // Adding class
+
+  allbtns[1].addEventListener("mousedown", function () {
+    allbtns[1].classList.add("btnActive");
+    allbtns[1].classList.add("fontActive");
+    allbtns[1].classList.add("greenActive");
+  });
+
+  // removing class
+
+  allbtns[1].addEventListener("mouseup", function () {
+    allbtns[1].classList.remove("btnActive");
+    allbtns[1].classList.remove("fontActive");
+    allbtns[1].classList.remove("greenActive");
+  });
+
+  // Adding class
+
+  allbtns[2].addEventListener("mousedown", function () {
+    allbtns[2].classList.add("btnActive");
+    allbtns[2].classList.add("fontActive");
+    allbtns[2].classList.add("greenActive");
+  });
+
+  // removing class
+
+  allbtns[2].addEventListener("mouseup", function () {
+    allbtns[2].classList.remove("btnActive");
+    allbtns[2].classList.remove("fontActive");
+    allbtns[2].classList.remove("greenActive");
+  });
+
+  // Adding class
+
+  allbtns[3].addEventListener("mousedown", function () {
+    allbtns[3].classList.add("btnActive");
+    allbtns[3].classList.add("fontActive");
+    allbtns[3].classList.add("greenActive");
+  });
+
+  // removing class
+
+  allbtns[3].addEventListener("mouseup", function () {
+    allbtns[3].classList.remove("btnActive");
+    allbtns[3].classList.remove("fontActive");
+    allbtns[3].classList.remove("greenActive");
+  });
+
+  // Adding class
+
+  allbtns[4].addEventListener("mousedown", function () {
+    allbtns[4].classList.add("btnActive");
+    allbtns[4].classList.add("fontActive");
+    allbtns[4].classList.add("greenActive");
+  });
+
+  // removing class
+
+  allbtns[4].addEventListener("mouseup", function () {
+    allbtns[4].classList.remove("btnActive");
+    allbtns[4].classList.remove("fontActive");
+    allbtns[4].classList.remove("greenActive");
+  });
+
+  // Adding class
+
+  allbtns[5].addEventListener("mousedown", function () {
+    allbtns[5].classList.add("btnActive");
+    allbtns[5].classList.add("fontActive");
+    allbtns[5].classList.add("greenActive");
+  });
+
+  // removing class
+
+  allbtns[5].addEventListener("mouseup", function () {
+    allbtns[5].classList.remove("btnActive");
+    allbtns[5].classList.remove("fontActive");
+    allbtns[5].classList.remove("greenActive");
+  });
+
+  // Adding class
+
+  allbtns[6].addEventListener("mousedown", function () {
+    allbtns[6].classList.add("btnActive");
+    allbtns[6].classList.add("fontActive");
+    allbtns[6].classList.add("greenActive");
+  });
+
+  // removing class
+
+  allbtns[6].addEventListener("mouseup", function () {
+    allbtns[6].classList.remove("btnActive");
+    allbtns[6].classList.remove("fontActive");
+    allbtns[6].classList.remove("greenActive");
+  });
+
+  // Adding class
+
+  allbtns[7].addEventListener("mousedown", function () {
+    allbtns[7].classList.add("btnActive");
+    allbtns[7].classList.add("fontActive");
+    allbtns[7].classList.add("greenActive");
+  });
+
+  // removing class
+
+  allbtns[7].addEventListener("mouseup", function () {
+    allbtns[7].classList.remove("btnActive");
+    allbtns[7].classList.remove("fontActive");
+    allbtns[7].classList.remove("greenActive");
+  });
+
+  // Adding class
+
+  allbtns[8].addEventListener("mousedown", function () {
+    allbtns[8].classList.add("btnActive");
+    allbtns[8].classList.add("fontActive");
+    allbtns[8].classList.add("greenActive");
+  });
+
+  // removing class
+
+  allbtns[8].addEventListener("mouseup", function () {
+    allbtns[8].classList.remove("btnActive");
+    allbtns[8].classList.remove("fontActive");
+    allbtns[8].classList.remove("greenActive");
+  });
+
+  // Adding class
+
+  allbtns[9].addEventListener("mousedown", function () {
+    allbtns[9].classList.add("btnActive");
+    allbtns[9].classList.add("fontActive");
+    allbtns[9].classList.add("greenActive");
+  });
+
+  // removing class
+
+  allbtns[9].addEventListener("mouseup", function () {
+    allbtns[9].classList.remove("btnActive");
+    allbtns[9].classList.remove("fontActive");
+    allbtns[9].classList.remove("greenActive");
+  });
+
+  // Adding class
+
+  allbtns[10].addEventListener("mousedown", function () {
+    allbtns[10].classList.add("btnActive");
+    allbtns[10].classList.add("fontActive");
+    allbtns[10].classList.add("greenActive");
+  });
+
+  // removing class
+
+  allbtns[10].addEventListener("mouseup", function () {
+    allbtns[10].classList.remove("btnActive");
+    allbtns[10].classList.remove("fontActive");
+    allbtns[10].classList.remove("greenActive");
+  });
+
+  // Adding class
+
+  allbtns[11].addEventListener("mousedown", function () {
+    allbtns[11].classList.add("btnActive");
+    allbtns[11].classList.add("fontActive");
+    allbtns[11].classList.add("greenActive");
+  });
+
+  // removing class
+
+  allbtns[11].addEventListener("mouseup", function () {
+    allbtns[11].classList.remove("btnActive");
+    allbtns[11].classList.remove("fontActive");
+    allbtns[11].classList.remove("greenActive");
+  });
+
+  // Adding class
+
+  allbtns[12].addEventListener("mousedown", function () {
+    allbtns[12].classList.add("btnActive");
+    allbtns[12].classList.add("fontActive");
+    allbtns[12].classList.add("greenActive");
+  });
+
+  // removing class
+
+  allbtns[12].addEventListener("mouseup", function () {
+    allbtns[12].classList.remove("btnActive");
+    allbtns[12].classList.remove("fontActive");
+    allbtns[12].classList.remove("greenActive");
+  });
+
+  // Adding class
+
+  allbtns[13].addEventListener("mousedown", function () {
+    allbtns[13].classList.add("btnActive");
+    allbtns[13].classList.add("fontActive");
+    allbtns[13].classList.add("greenActive");
+  });
+
+  // removing class
+
+  allbtns[13].addEventListener("mouseup", function () {
+    allbtns[13].classList.remove("btnActive");
+    allbtns[13].classList.remove("fontActive");
+    allbtns[13].classList.remove("greenActive");
+  });
+
+  // Adding class
+
+  allbtns[14].addEventListener("mousedown", function () {
+    allbtns[14].classList.add("btnActive");
+    allbtns[14].classList.add("fontActive");
+    allbtns[14].classList.add("greenActive");
+  });
+
+  // removing class
+
+  allbtns[14].addEventListener("mouseup", function () {
+    allbtns[14].classList.remove("btnActive");
+    allbtns[14].classList.remove("fontActive");
+    allbtns[14].classList.remove("greenActive");
+  });
+
+  allbtns[15].addEventListener("mousedown", function () {
+    allbtns[15].classList.add("btnActive");
+    allbtns[15].classList.add("fontActive");
+    allbtns[15].classList.add("greenActive");
+  });
+
+  // removing class
+
+  allbtns[15].addEventListener("mouseup", function () {
+    allbtns[15].classList.remove("btnActive");
+    allbtns[15].classList.remove("fontActive");
+      allbtns[15].classList.remove("greenActive");
+      console.log(allbtns[15])
+  });
+}
+
+
+
 
 
 
